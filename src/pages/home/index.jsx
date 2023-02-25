cba132e319fbbce1d5cbf553e8b186fdc12b62e6
unknown <eldirb95@gmail.com>
2023-02-25 19:11:41 +0700

    update all

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,23 +5,20 @@ import Footer from "../../layouts/footer";
 import styles from "./home.module.css";
 export const Home = (props) => {
   return (
-    <div className={styles.home}>
-      <div className={styles.absoluteItem}>
-        <div>
-          <button type="submit">Resume</button>
-        </div>
-        <div>
-          <button type="button">Contact</button>
-        </div>
+    <div className="container">
+      <div className={`row ${styles.absoluteItem}`}>
+        <button type="submit">Resume</button>
+        <button type="button">Contact</button>
       </div>
-      <div className={styles.profile}>
-        <div className={styles.profileItem}>
+
+      <div className={`row ${styles.profile}`}>
+        <div className={`row ${styles.profileLeft}`}>
           <p>Hi,</p>
           <h1>Eldir Buulolo</h1>
           <p>I am Software Development</p>
         </div>
-        <div className={styles.profileItem}>
-          <img className={styles.imgPhoto} src={photo} alt="profile" />
+        <div className={styles.profileRight}>
+          <img  src={photo} alt="profile" />
         </div>
       </div>
     </div>
--- src/pages/opensource/index.jsx
+++ src/pages/opensource/index.jsx
@@ -6,99 +6,43 @@ import styles from "./opensource.module.css";
 export const Opensource = (props) => {
   const [projects, setProjects] = useState([1, 2, 3, 4, 5]);
   return (
-    <div className={styles.osContainer}>
+    <div className="container">
       {projects.map((item, index) => {
         return (
-          <div className={styles.cardItem}>
-            {index % 2 == 0 ? (
-              <div
-                className={`${styles.content} ${
-                  index % 2 === 0 ? styles.contentmobile : styles.contentmobile
-                }`}
-              >
-                <img src={sample} />
-                <div
-                  className={
-                    index % 2 == 0 ? styles.contentDesc : styles.contentDescMin
-                  }
-                >
-                  <h1>Swipes Chat</h1>
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book. <br />
-                    <br />
-                    It has survived not only five centuries, but also the leap
-                    into electronic typesetting, remaining essentially
-                    unchanged. It was popularised in the 1960s with the release
-                    of Letraset sheets containing Lorem Ipsum passages, and more
-                    recently with desktop publishing software like Aldus
-                    PageMaker including versions of Lorem Ipsum.
-                  </p>
+          <div
+            className={`row ${
+              index % 2 == 0 ? styles.content : styles.contentReverce
+            }`}
+          >
+            <div className={styles.contentLeft}>
+              <h2>Swipes Chat</h2>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. <br />
+                <br />
+                It has survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.
+              </p>
 
-                  <div className={styles.projectUri}>
-                    <h5>Project URL : </h5>
-                    <a href="https://github.com/eldirb21/eldir-portofolio">
-                      eldir-portofolio
-                    </a>
-                  </div>
-                </div>
+              <div className={styles.url}>
+                <h5>Project URL : </h5>
+                <a href="https://github.com/eldirb21/eldir-portofolio">
+                  eldir-portofolio
+                </a>
               </div>
-            ) : (
-              <div className={styles.content}>
-                <div className={styles.contentDescMin}>
-                  <h1>Swipes Chat</h1>
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book. <br />
-                    <br />
-                    It has survived not only five centuries, but also the leap
-                    into electronic typesetting, remaining essentially
-                    unchanged. It was popularised in the 1960s with the release
-                    of Letraset sheets containing Lorem Ipsum passages, and more
-                    recently with desktop publishing software like Aldus
-                    PageMaker including versions of Lorem Ipsum.
-                  </p>
-
-                  <div className={styles.projectUri}>
-                    <h5>Project URL : </h5>
-                    <a href="https://github.com/eldirb21/eldir-portofolio">
-                      eldir-portofolio
-                    </a>
-                  </div>
-                </div>
-                <img src={sample} />
-              </div>
-            )}
+            </div>
+            <div className={styles.contentRight}>
+              <img src={sample} />
+            </div>
           </div>
         );
       })}
-
-      {/* <div className={(styles.cardItem, styles.cardItemRight)}>
-        <h1>Swipes Chat</h1>
-        <div>
-          <img src={sample} />
-          <div>
-            <p>
-              orem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-            </p>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
